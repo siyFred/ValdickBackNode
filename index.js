60886ac52f4c3b3108ab158ea9c1ff7ea86bb526
Friederich <siyfred1n@gmail.com>
2024-12-05 18:50:43 -0300

    d

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ export function getDatabase() {
 
 const app = express();
 app.use(express.static("uploads"));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 routes(app);
 
